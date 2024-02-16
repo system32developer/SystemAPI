0859a3225a0caf86b3f83672eb46baa7bd075e78
System32_Developer <80436979+system32developer@users.noreply.github.com>
2024-02-16 17:50:38 -0500

    Added some Comments to understand the code

--- src/main/java/com/system32/systemapi/utils/MessageUtils.java
+++ src/main/java/com/system32/systemapi/utils/MessageUtils.java
@@ -1,6 +1,5 @@
 package com.system32.systemapi.utils;
 
-import com.system32.systemapi.SystemAPI;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.*;
 import net.md_5.bungee.api.chat.hover.content.Item;
@@ -16,30 +15,67 @@ import java.util.regex.Pattern;
 import static com.system32.systemapi.SystemAPI.getPrefix;
 
 public class MessageUtils {
-    public static String color(String message){
+
+    /**
+     * Translates color codes in a string.
+     *
+     * @param message The string to translate color codes in.
+     * @return The string with color codes translated.
+     */
+    public static String color(String message) {
         Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
         Matcher match = pattern.matcher(message);
-        while(match.find()){
+        while (match.find()) {
             String color = message.substring(match.start(), match.end());
-            message = message.replace(color, ChatColor.of(color)+"");
+            message = message.replace(color, ChatColor.of(color) + "");
             match = pattern.matcher(message);
         }
-        return ChatColor.translateAlternateColorCodes('&', "&f"+message);
+        return ChatColor.translateAlternateColorCodes('&', "&f" + message);
     }
 
-    public static void msg(CommandSender sender, String message, Boolean prefix){
-        if(prefix) sender.sendMessage(color(getPrefix()+message));
-        else sender.sendMessage(color(message));
-    }
-    public static void msg(Player sender, String message, Boolean prefix){
-        if(prefix) sender.sendMessage(color(getPrefix()+message));
-        else sender.sendMessage(color(message));
-    }
-    public static void msg(ConsoleCommandSender sender, String message, Boolean prefix){
-        if(prefix) sender.sendMessage(color(getPrefix()+message));
+    /**
+     * Sends a message to a CommandSender.
+     *
+     * @param sender The CommandSender to send the message to.
+     * @param message The message to send.
+     * @param prefix Whether to prefix the message.
+     */
+    public static void msg(CommandSender sender, String message, Boolean prefix) {
+        if (prefix) sender.sendMessage(color(getPrefix() + message));
         else sender.sendMessage(color(message));
     }
 
+    /**
+     * Sends a message to a Player.
+     *
+     * @param sender The Player to send the message to.
+     * @param message The message to send.
+     * @param prefix Whether to prefix the message.
+     */
+    public static void msg(Player sender, String message, Boolean prefix) {
+        if (prefix) sender.sendMessage(color(getPrefix() + message));
+        else sender.sendMessage(color(message));
+    }
+
+    /**
+     * Sends a message to a ConsoleCommandSender.
+     *
+     * @param sender The ConsoleCommandSender to send the message to.
+     * @param message The message to send.
+     * @param prefix Whether to prefix the message.
+     */
+    public static void msg(ConsoleCommandSender sender, String message, Boolean prefix) {
+        if (prefix) sender.sendMessage(color(getPrefix() + message));
+        else sender.sendMessage(color(message));
+    }
+
+    /**
+     * Creates a TextComponent with a command suggestion.
+     *
+     * @param text The text of the TextComponent.
+     * @param value The command to suggest.
+     * @return The created TextComponent.
+     */
     public TextComponent msgCommandSuggest(String text, String value) {
 
         TextComponent textComponent = new TextComponent(color(text));
@@ -48,6 +84,14 @@ public class MessageUtils {
         return textComponent;
     }
 
+    /**
+     * Creates a TextComponent with a hover text and a command suggestion.
+     *
+     * @param text The text of the TextComponent.
+     * @param hover_msg The hover text.
+     * @param value The command to suggest.
+     * @return The created TextComponent.
+     */
     public TextComponent msgHover(String text, String hover_msg, String value) {
 
         TextComponent textComponent = new TextComponent(color(text));
@@ -56,22 +100,53 @@ public class MessageUtils {
         return textComponent;
     }
 
+    /**
+     * Creates a TextComponent with a command to run.
+     *
+     * @param text The text of the TextComponent.
+     * @param cmd The command to run.
+     * @return The created TextComponent.
+     */
     public TextComponent msgCmd(String text, String cmd) {
-
         TextComponent textComponent = new TextComponent(color(text));
         textComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, cmd));
-
         return textComponent;
     }
 
-    public static TextComponent msgItem(ItemStack item, String hoverText){
+    /**
+     * Creates a TextComponent with a URL to open.
+     *
+     * @param text The text of the TextComponent.
+     * @param url The URL to open.
+     * @return The created TextComponent.
+     */
+    public TextComponent msgURL(String text, String url) {
+        TextComponent textComponent = new TextComponent(color(text));
+        textComponent.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
+        return textComponent;
+    }
+
+    /**
+     * Creates a TextComponent with an item hover text.
+     *
+     * @param item The item to display.
+     * @param hoverText The hover text.
+     * @return The created TextComponent.
+     */
+    public static TextComponent msgItem(ItemStack item, String hoverText) {
         String nbt = item.hasItemMeta() ? item.getItemMeta().getAsString() : "{}";
         Item contents = new Item(item.getType().getKey().toString(), item.getAmount(), ItemTag.ofNbt(nbt));
         net.md_5.bungee.api.chat.TextComponent component = new TextComponent(color(hoverText));
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM, contents));
         return component;
     }
-    public static void log(String message){
-        Bukkit.getConsoleSender().sendMessage(color(getPrefix()+message));
+
+    /**
+     * Logs a message to the console.
+     *
+     * @param message The message to log.
+     */
+    public static void log(String message) {
+        Bukkit.getConsoleSender().sendMessage(color(getPrefix() + message));
     }
 }
